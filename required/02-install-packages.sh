f1b980951d4708a679951d202bca2dbc0cc17917
Italo Penaforte <italopenaforte@gmail.com>
2024-08-13 23:15:42 -0300

    add steps required

---
+++ required/02-install-packages.sh
@@ -0,0 +1,8 @@
+sudo apt install -y \
+  build-essential \
+  pkg-config \
+  autoconf \
+  git \
+  curl \
+  unzip \
+  gir1.2-gtop-2.0 gir1.2-clutter-1.0 # for system-monitor TopHat
---
+++ required/flatpak.sh
@@ -0,0 +1,3 @@
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
